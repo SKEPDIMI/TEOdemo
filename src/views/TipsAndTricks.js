f93b99f0a6772f525bc3f11827b38e706fa7361b
John S <diaz.john312@gmail.com>
2018-10-16 14:56:19 -0400

    added layout for tipsandtricks

--- src/views/TipsAndTricks.js
+++ src/views/TipsAndTricks.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native-elements';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { Text, Header, Icon } from 'react-native-elements';
+import {widthPercentageToDP as vw, heightPercentageToDP as vh} from 'react-native-responsive-screen';
 
 export default class TipsAndTricks extends Component {
   render() {
     return (
-      <Text>
-        Hey
-      </Text>
+      <ScrollView style={StyleSheet.mainContainer}>
+        <View
+          style={styles.intro}
+        >
+          <View
+            style={styles.header}
+          >
+            <Icon name="chevron-left" color="#222" size={52} onPress={() => this.props.navigation.goBack()}/>
+          </View>
+          <View
+            style={styles.carouselContainer}
+          >
+
+          </View>
+        </View>
+        <View
+          style={styles.content}
+        >
+        
+        </View>
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    alignItems: 'stretch',
+    backgroundColor: 'white',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 20,
+  },
+  carouselContainer: {
+    padding: 20,
+    flex: 1,
+  },
+  intro: {
+    minHeight: vh('63%'),
+
+    alignItems: 'stretch',
+    backgroundColor: 'white',
+  },
+  content: {
+    minHeight: vh('90%'),
+    backgroundColor: '#f2f2f2',
+  },
+})
