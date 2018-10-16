9ecbc6a0155691e98ac0c8c2710bf576f1bc77e6
John S <diaz.john312@gmail.com>
2018-10-16 13:30:24 -0400

    added tips and tricks view

---
+++ src/views/TipsAndTricks.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native-elements';
+
+export default class TipsAndTricks extends Component {
+  render() {
+    return (
+      <Text>
+        Hey
+      </Text>
+    )
+  }
+}
