b5b1b3b9801eadf75c3941a05dce9b85869ab28e
John S <diaz.john312@gmail.com>
2018-10-16 13:28:47 -0400

    renamed sessions and added content

---
+++ src/views/Tips.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Text, Button } from 'react-native-elements';
+
+export default class Tips extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text
+          fontFamily="Montserrat"
+          style={{
+            fontSize: 31,
+            marginBottom: 10
+          }}
+        >Welcome to Tips and Tricks!</Text>
+        <Button
+          title="Example of tips and tricks"
+          buttonStyle={{
+            backgroundColor: '#00D0D2',
+            borderRadius: 10
+          }}
+          containerViewStyle={{
+            marginLeft: 0
+          }}
+          iconRight={{
+            name: 'chevron-right'
+          }}
+          onPress={() => this.props.navigation.navigate('TipsAndTricks')}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 30,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#F5FCFF',
+  },
+});
